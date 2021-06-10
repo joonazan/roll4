b47f6ee2cd933d68e6cde75ab886d0f6253d5db1
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2021-06-11 00:56:56 +0300

    implement rerolling

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,5 @@
+#![recursion_limit = "256"]
+
 use aper::{StateMachineContainerProgram, Timestamp};
 use aper_yew::{ClientBuilder, View, ViewContext};
 use state::dice::{Dice, DiceTransition};
@@ -18,7 +20,7 @@ impl View for DiceView {
             <button onclick=context.callback.reform(move |_| Some(DiceTransition::Roll(n)))>{n}</button>
         });
         html! {<>
-            <DiceComponent timestamp=context.time rolls=state.0.rolls.clone() last_rolled=state.0.last_rolled.clone() />
+            <DiceComponent timestamp=context.time rolls=state.0.rolls.clone() last_rolled=state.0.last_rolled.clone() cb=context.callback.clone() />
             <div>
                 {"Roll: "}{for roll_buttons}
             </div>
@@ -32,7 +34,15 @@ fn main() {
 
 struct DiceComponent {
     last_update: Timestamp,
-    dice: Vec<(u8, bool)>,
+    dice: Vec<Die>,
+    link: ComponentLink<Self>,
+    cb: Callback<Option<DiceTransition>>,
+}
+
+struct Die {
+    roll: u8,
+    class: bool,
+    selected: bool,
 }
 
 #[derive(Properties, Clone)]
@@ -40,16 +50,32 @@ struct DiceProps {
     timestamp: Timestamp,
     rolls: Vec<u8>,
     last_rolled: Vec<bool>,
+    cb: Callback<Option<DiceTransition>>,
 }
 
+enum DiceMsg {
+    Select(usize),
+}
+use DiceMsg::*;
+
 impl Component for DiceComponent {
-    type Message = ();
+    type Message = DiceMsg;
     type Properties = DiceProps;
 
-    fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
         Self {
             last_update: props.timestamp,
-            dice: props.rolls.into_iter().zip(repeat(false)).collect(),
+            dice: props
+                .rolls
+                .into_iter()
+                .map(|r| Die {
+                    roll: r,
+                    class: false,
+                    selected: false,
+                })
+                .collect(),
+            link,
+            cb: props.cb,
         }
     }
 
@@ -62,27 +88,57 @@ impl Component for DiceComponent {
                 .zip(
                     self.dice
                         .iter()
-                        .map(|(_, c)| c)
+                        .map(|Die { class, .. }| class)
                         .zip(props.last_rolled.into_iter())
                         .map(|(&c, rolled)| if rolled { !c } else { c })
                         .chain(repeat(false)),
                 )
+                .map(|(r, c)| Die {
+                    roll: r,
+                    class: c,
+                    selected: false,
+                })
                 .collect();
+
             true
         } else {
             false
         }
     }
 
-    fn update(&mut self, _: ()) -> ShouldRender {
-        false
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Select(n) => {
+                self.dice[n].selected = !self.dice[n].selected;
+                true
+            }
+        }
     }
 
     fn view(&self) -> Html {
-        html! { <div>
-            {for self.dice.iter().map(|(x, c)|
-                html!{<span class=if *c {"die dieani1"} else {"die dieani2"}>{x}</span>}
+        let cannot_reroll = self.dice.iter().all(|d| d.selected == false);
+
+        html! { <>
+            <div>
+            {for self.dice.iter().enumerate().map(|(i, d)|
+                html!{<span data-selected=d.selected
+                      class=if d.class {"die dieani1"} else {"die dieani2"}
+                      onclick=self.link.callback(move |_| Select(i))>
+                      {d.roll}
+                </span>}
             )}
-        </div> }
+            </div>
+            <button disabled=cannot_reroll
+             onclick=self.cb.reform({
+                 let mask: Option<Vec<bool>> = if cannot_reroll {
+                     None
+                 }else {
+                     Some(self.dice.iter().map(|d| d.selected).collect())
+                 };
+                 move |_| mask.clone().map(DiceTransition::Reroll)
+             })>
+                {"Reroll"}
+            </button>
+        </>}
     }
 }
--- state/src/dice.rs
+++ state/src/dice.rs
@@ -1,14 +1,14 @@
 use aper::{StateMachine, Transition};
-use serde::{Deserialize, Serialize};
-use rand::SeedableRng;
 use rand::distributions::{Distribution, Uniform};
+use rand::SeedableRng;
 use rand_chacha::ChaCha12Rng;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Dice {
     pub rolls: Vec<u8>,
     pub last_rolled: Vec<bool>,
-    rng: ChaCha12Rng
+    rng: ChaCha12Rng,
 }
 
 impl Default for Dice {
@@ -23,18 +23,29 @@ impl Default for Dice {
 
 #[derive(Transition, Serialize, Deserialize, Debug, Clone, PartialEq)]
 pub enum DiceTransition {
-    Roll(u8)
+    Roll(u8),
+    Reroll(Vec<bool>),
 }
 use DiceTransition::*;
 
 impl StateMachine for Dice {
     type Transition = DiceTransition;
     fn apply(&mut self, t: Self::Transition) {
+        let d4 = Uniform::from(1..=4);
         match t {
             Roll(x) => {
-                let d4 = Uniform::from(1..=4);
                 self.rolls = (0..x).map(|_| d4.sample(&mut self.rng)).collect();
                 self.last_rolled = vec![true; x as usize];
+            }
+            Reroll(mask) => {
+                let rng = &mut self.rng;
+                self.rolls = self
+                    .rolls
+                    .iter()
+                    .zip(&mask)
+                    .map(|(r, m)| if *m { d4.sample(rng) } else { *r })
+                    .collect();
+                self.last_rolled = mask;
             }
         }
     }
