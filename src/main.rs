c53e6f0ab0d5012db5b0767cc08e6151e390a711
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2021-06-23 14:28:48 +0300

    serve on localhost unless given a CLI argument

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@ use aper_actix::ServerBuilder;
 use state::Game;
 
 fn main() -> std::io::Result<()> {
-    ServerBuilder::new(StateMachineContainerProgram(Game::default())).serve()
+    let builder = ServerBuilder::new(StateMachineContainerProgram(Game::default()));
+    if std::env::args().len() > 1 {
+        builder.serve_on("0.0.0.0", 666)
+    } else {
+        builder.serve()
+    }
 }
